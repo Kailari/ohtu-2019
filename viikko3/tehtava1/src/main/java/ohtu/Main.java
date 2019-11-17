9a3b1aab10a3ce9c45e94ef6dac398f45e91c846
Ilari Tommiska <ilari97@hotmail.com>
2019-11-17 07:46:37 +0200

    Printataan kaikki suomalaiset pelaajat

--- viikko3/tehtava1/src/main/java/ohtu/Main.java
+++ viikko3/tehtava1/src/main/java/ohtu/Main.java
@@ -1,8 +1,12 @@
 package ohtu;
 
 import com.google.gson.Gson;
-import java.io.IOException;
 import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,16 +14,25 @@ public class Main {
 
         String bodyText = Request.Get(url).execute().returnContent().asString();
 
-        System.out.println("json-muotoinen data:");
-        System.out.println( bodyText );
-
         Gson mapper = new Gson();
         Player[] players = mapper.fromJson(bodyText, Player[].class);
 
-        System.out.println("Oliot:");
-        for (Player player : players) {
-            System.out.println(player);
-        }
+        System.out.println(String.format("Players from FIN %s\n", new Date()));
+        Arrays.stream(players)
+              .filter(p -> p.getNationality().equalsIgnoreCase("FIN"))
+              .sorted(Comparator.comparingInt(Player::getPoints).reversed())
+              .forEach(Main::printPlayer);
+    }
+
+    private static void printPlayer(Player player) {
+        System.out.println(String.format(
+                "%-20s %s  %2d + %2d = %2d ",
+                player.getName(),
+                player.getTeam(),
+                player.getGoals(),
+                player.getAssists(),
+                player.getPoints()
+        ));
     }
 
 }
--- viikko3/tehtava1/src/main/java/ohtu/Player.java
+++ viikko3/tehtava1/src/main/java/ohtu/Player.java
@@ -1,15 +1,47 @@
 
 package ohtu;
 
-public class Player {
-    private String name;
+import java.util.Date;
 
-    public void setName(String name) {
-        this.name = name;
+public class Player {
+    private int goals;
+    private int assists;
+    private int penalties;
+    private String name;
+    private String team;
+    private String nationality;
+    private Date birthdate;
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public int getPoints() {
+        return goals + assists;
+    }
+
+    public int getPenalties() {
+        return penalties;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
     }
 
     @Override
